06b4a1b089c4e257d1375803179cbcf3936a0a9a
Berg <dberg@engr-131-04.eastern.ewu.edu>
2014-03-16 16:52:26 -0700

    code test

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
     // Get _weather_ object
     success: function(weather) {
       
-      var code = weather.code;
+      var code = '<h2><i class="icon-' + weather.code ></i>;
       var temp = weather.temp + '&deg;' + weather.units.temp;
       var city = weather.city + ', ' + weather.region;
       var currently = weather.currently;
