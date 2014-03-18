3b41a8151ff4d173819c5a1ae0e4e6aa0776fcab
Berg <dberg@engr-131-04.eastern.ewu.edu>
2014-03-17 18:47:32 -0700

    attempt

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,16 @@ $(document).foundation();
 
 $(document).ready(function(){
 
-$.simpleWeather({
+  // 1. Check for Geolocation
+  // if (navigator.geolocation) {
+  //   $('.getGeolocation').show(); 
+  // } else {
+  //   $('.getGeolocation').hide();
+  // }
+
+ });
+
+  $.simpleWeather({
     
     location: '99004', // change zip
     unit: 'f',
@@ -32,7 +41,40 @@ $.simpleWeather({
   });
 
 
-)};
+  $('.getGeolocation').on('click', function() {
+  
+    navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
+  
+  var loadWeather = function(location) {
+    
+    $.simpleWeather({
+    location: location,
+
+    success: function(weather) {
+      
+      var code = '<img src=' + weather.image + '>';
+      var temp = weather.temp + '&deg;' + weather.units.temp;
+      var city = weather.city + ', ' + weather.region;
+      var sunrise = 'Sunrise ' + weather.sunrise;
+      var high = 'High ' + weather.high;
+      var low = 'Low ' + weather.low;
+      var sunset = 'Sunset ' + weather.sunset;
+      
+      $('.code').html(code);
+      $('.temp').html(temp);
+      $('.city').text(city);
+      $('.sunrise').text(sunrise);
+      $('.high').text(high);
+      $('.low').text(low);
+      $('.sunset').text(sunset);
+
+      console.log(weather);
+    }
+
+  });
 
 $('.getWeather').on('click', function() {
   
