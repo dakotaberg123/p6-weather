bad0c5a5b6754bce53a264f54443a400bac1661c
Berg <dberg@engr-131-04.eastern.ewu.edu>
2014-03-17 18:57:38 -0700

    reverted

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 $(document).foundation();
 
 $(document).ready(function(){
@@ -9,21 +8,63 @@ $(document).ready(function(){
   // } else {
   //   $('.getGeolocation').hide();
   // }
-
  });
 
+  $.simpleWeather({
+    
+    location: '99004', // change zip
+    unit: 'f',
+    
+    // Get _weather_ object
+    success: function(weather) {
+      
+      var code = '<img src=' + weather.image + '>';
+      var temp = weather.temp + '&deg;' + weather.units.temp;
+      var city = weather.city + ', ' + weather.region;
+      var sunrise = 'Sunrise ' + weather.sunrise;
+      var high = 'High ' + weather.high;
+      var low = 'Low ' + weather.low;
+      var sunset = 'Sunset ' + weather.sunset;
+      
+      $('.code').html(code);
+      $('.temp').html(temp);
+      $('.city').text(city);
+      $('.sunrise').text(sunrise);
+      $('.high').text(high);
+      $('.low').text(low);
+      $('.sunset').text(sunset);
+      
+      // See console for _weather_ object
+      console.log(weather);
+    },
+  
+    // if error
+    error: function(error) {  
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+
+
+  // 2. Get Geolocation & return Simple Weather
   $('.getGeolocation').on('click', function() {
   
     navigator.geolocation.getCurrentPosition(function(position) {
+    //load weather using your lat/lng coordinates. See _loadWeather()_ below
     loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    // See latitute & longitude. Note, wait a few seconds
     console.log(position.coords.latitude+','+position.coords.longitude);
   });
   
+ 
+
+  // 3. Wrap SimpleWeather in a function called _loadWeather()
   var loadWeather = function(location) {
     
     $.simpleWeather({
     location: location,
-
+    
+    // Get _weather_ object
     success: function(weather) {
       
       var code = '<img src=' + weather.image + '>';
@@ -42,15 +83,23 @@ $(document).ready(function(){
       $('.low').text(low);
       $('.sunset').text(sunset);
 
+      
+      // See console for _weather_ object
       console.log(weather);
     }
 
   });
 
+    // REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+
+// On click button, get zip, then run Simple Weather
 $('.getWeather').on('click', function() {
   
+  // 1. Get & store entered zipcode
   var zipcode = $('#getWeather').val();
-
+  
+  // 2. Pass weather into _simpleWeather()_ object
   $.simpleWeather({
     
     location: zipcode,
@@ -73,6 +122,7 @@ $('.getWeather').on('click', function() {
       $('.low').text(low);
       $('.sunset').text(sunset);
 
+      // See console for all properties of object
       console.log(weather);
     },
   
@@ -82,11 +132,14 @@ $('.getWeather').on('click', function() {
   
   });
   
+  // 3. Reset input value
   $('#getWeather').val('');
   
 });
 
+
+   
     
-  }; 
+  }; // end of _loadWeather()_ function
 
 }); 
