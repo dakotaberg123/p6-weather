5a92bbdbb2c86f0817cf4b93191e87eaf782fa64
Berg <dberg@engr-131-04.eastern.ewu.edu>
2014-03-17 17:03:16 -0700

    geo back

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,56 @@ $(document).foundation();
 
 $(document).ready(function(){
 
- 
+  // 1. Check for Geolocation
+  if (navigator.geolocation) {
+    $('.getGeolocation').show(); 
+  } else {
+    $('.getGeolocation').hide();
+  }
+
+  // 2. Get Geolocation & return Simple Weather
+  $('.getGeolocation').on('click', function() {
+  
+    navigator.geolocation.getCurrentPosition(function(position) {
+    //load weather using your lat/lng coordinates. See _loadWeather()_ below
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    // See latitute & longitude. Note, wait a few seconds
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
+  
+  });
+
+  // 3. Wrap SimpleWeather in a function called _loadWeather()
+  var loadWeather = function(location) {
+    
+    $.simpleWeather({
+    location: location,
+    
+    // Get _weather_ object
+    success: function(weather) {
+      
+      var code = '<img src=' + weather.image + '>';
+      var temp = weather.temp + '&deg;' + weather.units.temp;
+      var city = weather.city + ', ' + weather.region;
+      var sunrise = 'Sunrise ' + weather.sunrise;
+      var high = 'High ' + weather.high;
+      var low = 'Low ' + weather.low;
+      var sunset = 'Sunset ' + weather.sunset;
+      
+      $('.code').html(code);
+      $('.temp').html(temp);
+      $('.city').text(city);
+      $('.sunrise').text(sunrise);
+      $('.high').text(high);
+      $('.low').text(low);
+      $('.sunset').text(sunset);
+
+      
+      // See console for _weather_ object
+      console.log(weather);
+    }
+
+  });
 
     // REF: http://foundation.zurb.com/docs/
 // REF: http://simpleweatherjs.com/
