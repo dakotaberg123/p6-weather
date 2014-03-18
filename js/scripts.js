31f241ae5b6dd06a79b4f4141bb347eb3a47b72f
Berg <dberg@engr-131-04.eastern.ewu.edu>
2014-03-17 19:09:18 -0700

    search works

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,10 @@
+
 $(document).foundation();
 
-$(document).ready(function(){
-
-  // 1. Check for Geolocation
-  // if (navigator.geolocation) {
-  //   $('.getGeolocation').show(); 
-  // } else {
-  //   $('.getGeolocation').hide();
-  // }
- });
-
+  $('.getWeather').on('click', function() {
   
-
-
-  // 2. Get Geolocation & return Simple Weather
-  $('.getGeolocation').on('click', function() {
-  
-    navigator.geolocation.getCurrentPosition(function(position) {
-    //load weather using your lat/lng coordinates. See _loadWeather()_ below
-    loadWeather(position.coords.latitude+','+position.coords.longitude); 
-    // See latitute & longitude. Note, wait a few seconds
-    console.log(position.coords.latitude+','+position.coords.longitude);
-  });
-  
- 
-
-  // 3. Wrap SimpleWeather in a function called _loadWeather()
-  var loadWeather = function(location) {
-    
-    $.simpleWeather({
-    location: location,
-    
-    // Get _weather_ object
-    success: function(weather) {
-      
-      var code = '<img src=' + weather.image + '>';
-      var temp = weather.temp + '&deg;' + weather.units.temp;
-      var city = weather.city + ', ' + weather.region;
-      var sunrise = 'Sunrise ' + weather.sunrise;
-      var high = 'High ' + weather.high;
-      var low = 'Low ' + weather.low;
-      var sunset = 'Sunset ' + weather.sunset;
-      
-      $('.code').html(code);
-      $('.temp').html(temp);
-      $('.city').text(city);
-      $('.sunrise').text(sunrise);
-      $('.high').text(high);
-      $('.low').text(low);
-      $('.sunset').text(sunset);
-
-      
-      // See console for _weather_ object
-      console.log(weather);
-    }
-
-  });
-
-    // REF: http://foundation.zurb.com/docs/
-// REF: http://simpleweatherjs.com/
-
-// On click button, get zip, then run Simple Weather
-$('.getWeather').on('click', function() {
-  
-  // 1. Get & store entered zipcode
   var zipcode = $('#getWeather').val();
   
-  // 2. Pass weather into _simpleWeather()_ object
   $.simpleWeather({
     
     location: zipcode,
@@ -89,7 +27,7 @@ $('.getWeather').on('click', function() {
       $('.low').text(low);
       $('.sunset').text(sunset);
 
-      // See console for all properties of object
+
       console.log(weather);
     },
   
@@ -99,14 +37,9 @@ $('.getWeather').on('click', function() {
   
   });
   
-  // 3. Reset input value
   $('#getWeather').val('');
   
 });
 
 
-   
-    
-  }; // end of _loadWeather()_ function
 
-}); 
